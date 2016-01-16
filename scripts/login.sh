c3dca69c71310291683560893c1363b3425f7edc
Jon Gonzales <jmgdrumin@gmail.com>
2016-01-15 18:58:11 -0800

    added script to launch server at login, and another to launch barc (python) virtualenv each time user opens shell

--- scripts/login.sh
+++ scripts/login.sh
@@ -3,4 +3,10 @@
 
 source /usr/local/bin/virtualenvwrapper.sh
 export WORKON_HOME=$HOME/.virtualenvs
+
+
+cd /home/odroid/barc/Dator
 workon barc
+supervisord -c /home/odroid/barc/Dator/supervisor.conf
+cd
+deactivate
