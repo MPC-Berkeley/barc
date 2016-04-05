3fe07a8e0722aa4f8cb73866aac49dfd924112a6
Jon Gonzales <jmgdrumin@gmail.com>
2016-04-05 16:51:08 -0700

    Fixed base.py for environment variables not set

--- workspace/src/data_service/scripts/base.py
+++ workspace/src/data_service/scripts/base.py
@@ -23,10 +23,7 @@ class Configurator(object):
             self.config = {
                 # ENVIRONMENT VARIBALE OR ROS ENVIRONMENT VARIABLE
                 'name': os.environ['TEAM_NAME'],
-#                'server': 'http://104.131.132.100:8009',
-                'server': 'http://localhost:8000',
-#               'server': 'http://localhost:8888',
-#                'server': 'http://ec2-52-22-139-24.compute-1.amazonaws.com/',
+                'server': os.environ['DATOR_SERVER'],
                 'secret_uuid': str(uuid4()),
                 'registration_token': 'abcd',
                 'id': "1"
