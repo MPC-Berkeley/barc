d4828a55a7abbdfd9b045648d3d4436d6f29a8a4
Kiet Lam <kiet.lam@berkeley.edu>
2016-04-06 12:02:50 -0700

    Fixed upload script

--- workspace/src/data_service/scripts/upload.py
+++ workspace/src/data_service/scripts/upload.py
@@ -3,12 +3,12 @@
 import os, sys
 import time
 
-os.chdir('/home/odroid/barc/workspace/src/data_service/scripts')
+os.chdir('/home/mpc/barc/workspace/src/data_service/scripts')
 
 from data_connection import *
 from base import *
 
-proj_path = '/home/odroid/barc/Dator'
+proj_path = '/home/mpc/barc/Dator'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dator.settings')
 sys.path.append(proj_path)
@@ -19,7 +19,7 @@ application = get_wsgi_application()
 
 from data_api.models import *
 
-CLOUD_CONFIG_LOCATION = '/home/odroid/cloud.cfg'
+CLOUD_CONFIG_LOCATION = '/home/mpc/cloud.cfg'
 
 if __name__ == '__main__':
     configurator = init_configurator(CLOUD_CONFIG_LOCATION)
@@ -29,18 +29,21 @@ if __name__ == '__main__':
         try:
             experiment = data_connection.get_or_create_experiment(sig.experiment.name)
             signal = data_connection.get_or_create_signal(sig.name, experiment)
+            print signal
 
             try:
-                lst = LocalSignalTag.objects.get(signal=signal)
-            except LocalSignalTag.DoesNotExist:
+                lst = LocalSignalTag.objects.filter(signal__name=sig.name, signal__experiment__name=sig.experiment.name)[0]
+            except (LocalSignalTag.DoesNotExist, IndexError) as e:
                 lst = LocalSignalTag()
-                lst.signal = signal
+                lst.signal = sig
                 lst.uploaded = False
 
             if not lst.uploaded:
+                print 'Uploading'
                 data_connection.add_signal_points(signal['id'], sig.get_data())
                 lst.uploaded = True
                 lst.save()
+                print 'Finished Uploading'
 
             lst.save()
 
