6e6cee24531aaf08b8df5a53983f8914c031ff3a
Greg Marcil <grmarcil@gmail.com>
2016-05-04 02:27:32 -0700

    Adapt state estimators to work with only one front encoder

--- workspace/src/barc/src/state_estimation_DynBkMdl.py
+++ workspace/src/barc/src/state_estimation_DynBkMdl.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 # ---------------------------------------------------------------------------
-# Licensing Information: You are free to use or extend these projects for 
+# Licensing Information: You are free to use or extend these projects for
 # education or reserach purposes provided that (1) you retain this notice
-# and (2) you provide clear attribution to UC Berkeley, including a link 
+# and (2) you provide clear attribution to UC Berkeley, including a link
 # to http://barc-project.com
 #
 # Attibution Information: The barc project ROS code-base was developed
 # at UC Berkeley in the Model Predictive Control (MPC) lab by Jon Gonzales
 # (jon.gonzales@berkeley.edu). The cloud services integation with ROS was developed
-# by Kiet Lam  (kiet.lam@berkeley.edu). The web-server app Dator was 
+# by Kiet Lam  (kiet.lam@berkeley.edu). The web-server app Dator was
 # based on an open source project by Bruce Wootton
 # ---------------------------------------------------------------------------
 
@@ -28,7 +28,7 @@ from rospy_tutorials.msg import Floats
 from rospy.numpy_msg import numpy_msg
 
 # input variables
-d_f 	    = 0
+d_f         = 0
 servo_pwm   = 90
 motor_pwm   = 90
 
@@ -36,69 +36,71 @@ motor_pwm   = 90
 (roll, pitch, yaw, a_x, a_y, a_z, w_x, w_y, w_z) = zeros(9)
 
 # from encoder
-v_x_enc 	= 0
-t0 	        = time.time()
-n_FL	    = 0                     # counts in the front left tire
-n_FR 	    = 0                     # counts in the front right tire
-n_FL_prev 	= 0
-n_FR_prev 	= 0
-r_tire 		= 0.04                  # radius from tire center to perimeter along magnets [m]
-dx_qrt 	    = 2.0*pi*r_tire/4.0     # distance along quarter tire edge
+v_x_enc     = 0
+t0          = time.time()
+n_FL        = 0                     # counts in the front left tire
+n_FR        = 0                     # counts in the front right tire
+n_FL_prev   = 0
+n_FR_prev   = 0
+r_tire      = 0.04                  # radius from tire center to perimeter along magnets [m]
+dx_qrt      = 2.0*pi*r_tire/4.0     # distance along quarter tire edge
 
 # ecu command update
 def ecu_callback(data):
     global motor_pwm, servo_pwm, d_f
-    motor_pwm	    = data.motor_pwm
+    motor_pwm       = data.motor_pwm
     servo_pwm       = data.servo_pwm
     d_f             = servo_2_angle(servo_pwm)
 
 # imu measurement update
 def imu_callback(data):
     # units: [rad] and [rad/s]
-	global roll, pitch, yaw, a_x, a_y, a_z, w_x, w_y, w_z
-	(roll, pitch, yaw, a_x, a_y, a_z, w_x, w_y, w_z) = data.value
+    global roll, pitch, yaw, a_x, a_y, a_z, w_x, w_y, w_z
+    (roll, pitch, yaw, a_x, a_y, a_z, w_x, w_y, w_z) = data.value
 
 # encoder measurement update
 def enc_callback(data):
-	global v_x_enc, d_f, t0
-	global n_FL, n_FR, n_FL_prev, n_FR_prev
+    global v_x_enc, d_f, t0
+    global n_FL, n_FR, n_FL_prev, n_FR_prev
 
-	n_FL = data.FL
-	n_FR = data.FR
+    n_FL = data.FL
+    n_FR = data.FR
 
-	# compute time elapsed
-	tf = time.time()
-	dt = tf - t0
-	
-	# if enough time elapse has elapsed, estimate v_x
-	dt_min = 0.20
-	if dt >= dt_min:
-		# compute speed :  speed = distance / time
-		v_FL = float(n_FL- n_FL_prev)*dx_qrt/dt
-		v_FR = float(n_FR- n_FR_prev)*dx_qrt/dt
+    # compute time elapsed
+    tf = time.time()
+    dt = tf - t0
 
-		# update encoder v_x, v_y measurements
-		# only valid for small slip angles, still valid for drift?
-		v_x_enc 	= (v_FL + v_FR)/2.0*cos(d_f)
+    # if enough time elapse has elapsed, estimate v_x
+    dt_min = 0.20
+    if dt >= dt_min:
+        # compute speed :  speed = distance / time
+        v_FL = float(n_FL- n_FL_prev)*dx_qrt/dt
+        v_FR = float(n_FR- n_FR_prev)*dx_qrt/dt
 
-		# update old data
-		n_FL_prev   = n_FL
-		n_FR_prev   = n_FR
-		t0 	        = time.time()
+        # update encoder v_x, v_y measurements
+        # only valid for small slip angles, still valid for drift?
+        # v_x_enc     = (v_FL + v_FR)/2.0*cos(d_f)
+        # My modification for single front encoder
+        v_x_enc     = v_FL*cos(d_f)
+
+        # update old data
+        n_FL_prev   = n_FL
+        n_FR_prev   = n_FR
+        t0          = time.time()
 
 
 # state estimation node
 def state_estimation():
-	  # initialize node
+      # initialize node
     rospy.init_node('state_estimation', anonymous=True)
 
     # topic subscriptions / publications
     rospy.Subscriber('imu', TimeData, imu_callback)
     rospy.Subscriber('encoder', Encoder, enc_callback)
     rospy.Subscriber('ecu', ECU, ecu_callback)
-    state_pub 	= rospy.Publisher('state_estimate', Vector3, queue_size = 10)
+    state_pub   = rospy.Publisher('state_estimate', Vector3, queue_size = 10)
 
-	# get vehicle dimension parameters
+    # get vehicle dimension parameters
     L_a = rospy.get_param("state_estimation/L_a")       # distance from CoG to front axel
     L_b = rospy.get_param("state_estimation/L_b")       # distance from CoG to rear axel
     m   = rospy.get_param("state_estimation/m")         # mass of vehicle
@@ -123,11 +125,11 @@ def state_estimation():
     r_std   = rospy.get_param("state_estimation/r_std")             # std of measurementnoise
     v_x_min     = rospy.get_param("state_estimation/v_x_min")  # minimum velociy before using EKF
 
-	# set node rate
-    loop_rate 	= 50
-    dt 		    = 1.0 / loop_rate
-    rate 		= rospy.Rate(loop_rate)
-    t0 				= time.time()
+    # set node rate
+    loop_rate   = 50
+    dt          = 1.0 / loop_rate
+    rate        = rospy.Rate(loop_rate)
+    t0              = time.time()
 
     # estimation variables for Luemberger observer
     z_EKF       = array([1.0, 0.0, 0.0])
@@ -143,7 +145,7 @@ def state_estimation():
 
     while not rospy.is_shutdown():
 
-		# publish state estimate
+        # publish state estimate
         (v_x, v_y, r) = z_EKF           # note, r = EKF estimate yaw rate
 
         # publish information
@@ -151,14 +153,14 @@ def state_estimation():
 
         # update filtered signal
         v_x_filt.update(v_x_enc)
-        v_x_est = v_x_filt.getFilteredSignal() 
+        v_x_est = v_x_filt.getFilteredSignal()
 
         # apply EKF
         if v_x_est > v_x_min:
             # get measurement
             y = array([v_x_est, w_z])
 
-            # compute input motor signal 
+            # compute input motor signal
             if motor_pwm >= 95:
                 FxR     = (motor_pwm - 95)*0.3*m        # mapping from motor pwm to input force FxR
             else:
@@ -168,8 +170,8 @@ def state_estimation():
             u       = array([ d_f, FxR ])
 
             # build extra arguments for non-linear function
-            F_ext = array([ a0, Ff ]) 
-            args = (u, vhMdl, TrMdl, F_ext, dt) 
+            F_ext = array([ a0, Ff ])
+            args = (u, vhMdl, TrMdl, F_ext, dt)
 
             # apply EKF and get each state estimate
             (z_EKF,P) = ekf(f_3s, z_EKF, P, h_3s, y, Q, R, args )
@@ -177,12 +179,12 @@ def state_estimation():
         else:
             z_EKF[0] = float(v_x_enc)
             z_EKF[2] = float(w_z)
-        
-		# wait
+
+        # wait
         rate.sleep()
 
 if __name__ == '__main__':
-	try:
-	   state_estimation()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+       state_estimation()
+    except rospy.ROSInterruptException:
+        pass
--- workspace/src/barc/src/state_estimation_KinBkMdl.py
+++ workspace/src/barc/src/state_estimation_KinBkMdl.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 # ---------------------------------------------------------------------------
-# Licensing Information: You are free to use or extend these projects for 
+# Licensing Information: You are free to use or extend these projects for
 # education or reserach purposes provided that (1) you retain this notice
-# and (2) you provide clear attribution to UC Berkeley, including a link 
+# and (2) you provide clear attribution to UC Berkeley, including a link
 # to http://barc-project.com
 #
 # Attibution Information: The barc project ROS code-base was developed
 # at UC Berkeley in the Model Predictive Control (MPC) lab by Jon Gonzales
 # (jon.gonzales@berkeley.edu). The cloud services integation with ROS was developed
-# by Kiet Lam  (kiet.lam@berkeley.edu). The web-server app Dator was 
+# by Kiet Lam  (kiet.lam@berkeley.edu). The web-server app Dator was
 # based on an open source project by Bruce Wootton
 # ---------------------------------------------------------------------------
 
@@ -27,7 +27,7 @@ from rospy_tutorials.msg import Floats
 from rospy.numpy_msg import numpy_msg
 
 # input variables [default values]
-d_f 	    = 0         # steering angle [deg]
+d_f         = 0         # steering angle [deg]
 a           = 0         # acceleration [m/s]
 servo_pwm   = 90
 motor_pwm   = 90
@@ -38,19 +38,19 @@ yaw_prev    = 0
 psi         = 0
 
 # from encoder
-v 	        = 0
-t0 	        = time.time()
-n_FL	    = 0                     # counts in the front left tire
-n_FR 	    = 0                     # counts in the front right tire
-n_FL_prev 	= 0
-n_FR_prev 	= 0
-r_tire 		= 0.04                  # radius from tire center to perimeter along magnets [m]
-dx_qrt 	    = 2.0*pi*r_tire/4.0     # distance along quarter tire edge [m]
+v           = 0
+t0          = time.time()
+n_FL        = 0                     # counts in the front left tire
+n_FR        = 0                     # counts in the front right tire
+n_FL_prev   = 0
+n_FR_prev   = 0
+r_tire      = 0.036                  # radius from tire center to perimeter along magnets [m]
+dx_qrt      = 2.0*pi*r_tire/4.0     # distance along quarter tire edge [m]
 
 # ecu command update
 def ecu_callback(data):
     global motor_pwm, servo_pwm, d_f, a
-    motor_pwm	    = data.motor_pwm
+    motor_pwm       = data.motor_pwm
     servo_pwm       = data.servo_pwm
     d_f             = servo_2_angle(servo_pwm) * pi/180         # [rad]
 
@@ -73,42 +73,44 @@ def imu_callback(data):
 
 # encoder measurement update
 def enc_callback(data):
-	global v, d_f, t0
-	global n_FL, n_FR, n_FL_prev, n_FR_prev
+    global v, d_f, t0
+    global n_FL, n_FR, n_FL_prev, n_FR_prev
 
-	n_FL = data.FL
-	n_FR = data.FR
+    n_FL = data.FL
+    n_FR = data.FR
 
-	# compute time elapsed
-	tf = time.time()
-	dt = tf - t0
-	
-	# if enough time elapse has elapsed, estimate v_x
-	dt_min = 0.20
-	if dt >= dt_min:
-		# compute speed :  speed = distance / time
-		v_FL = float(n_FL- n_FL_prev)*dx_qrt/dt
-		v_FR = float(n_FR- n_FR_prev)*dx_qrt/dt
-		v 	 = (v_FL + v_FR)/2.0
+    # compute time elapsed
+    tf = time.time()
+    dt = tf - t0
 
-		# update old data
-		n_FL_prev   = n_FL
-		n_FR_prev   = n_FR
-		t0 	        = time.time()
+    # if enough time elapse has elapsed, estimate v_x
+    dt_min = 0.20
+    if dt >= dt_min:
+        # compute speed :  speed = distance / time
+        v_FL = float(n_FL- n_FL_prev)*dx_qrt/dt
+        v_FR = float(n_FR- n_FR_prev)*dx_qrt/dt
+        # v    = (v_FL + v_FR)/2.0
+        # My modification for single FL encoder
+        v = v_FL
+
+        # update old data
+        n_FL_prev   = n_FL
+        n_FR_prev   = n_FR
+        t0          = time.time()
 
 
 # state estimation node
 def state_estimation():
-	# initialize node
+    # initialize node
     rospy.init_node('state_estimation', anonymous=True)
 
     # topic subscriptions / publications
     rospy.Subscriber('imu', TimeData, imu_callback)
     rospy.Subscriber('encoder', Encoder, enc_callback)
     rospy.Subscriber('ecu', ECU, ecu_callback)
-    state_pub 	= rospy.Publisher('state_estimate', Z_KinBkMdl, queue_size = 10)
+    state_pub   = rospy.Publisher('state_estimate', Z_KinBkMdl, queue_size = 10)
 
-	# get vehicle dimension parameters
+    # get vehicle dimension parameters
     L_a = rospy.get_param("state_estimation/L_a")       # distance from CoG to front axel
     L_b = rospy.get_param("state_estimation/L_b")       # distance from CoG to rear axel
     vhMdl   = (L_a, L_b)
@@ -120,14 +122,14 @@ def state_estimation():
     q_std   = rospy.get_param("state_estimation/q_std")             # std of process noise
     r_std   = rospy.get_param("state_estimation/r_std")             # std of measurementnoise
 
-	# set node rate
-    loop_rate 	= 50
-    dt 		    = 1.0 / loop_rate
-    rate 		= rospy.Rate(loop_rate)
-    t0 			= time.time()
+    # set node rate
+    loop_rate   = 50
+    dt          = 1.0 / loop_rate
+    rate        = rospy.Rate(loop_rate)
+    t0          = time.time()
 
     # estimation variables for Luemberger observer
-    z_EKF       = zeros(4) 
+    z_EKF       = zeros(4)
 
     # estimation variables for EKF
     P           = eye(4)                # initial dynamics coveriance matrix
@@ -136,8 +138,8 @@ def state_estimation():
 
     while not rospy.is_shutdown():
 
-		# publish state estimate
-        (x, y, psi, v) = z_EKF          
+        # publish state estimate
+        (x, y, psi, v) = z_EKF
 
         # publish information
         state_pub.publish( Z_KinBkMdl(x, y, psi, v) )
@@ -146,16 +148,16 @@ def state_estimation():
         # collect inputs
         y   = array([psi, v])
         u   = array([ d_f, a ])
-        args = (u,vhMdl,dt) 
+        args = (u,vhMdl,dt)
 
         # apply EKF and get each state estimate
         (z_EKF,P) = ekf(f_KinBkMdl, z_EKF, P, h_KinBkMdl, y, Q, R, args )
 
-		# wait
+        # wait
         rate.sleep()
 
 if __name__ == '__main__':
-	try:
-	   state_estimation()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+       state_estimation()
+    except rospy.ROSInterruptException:
+        pass
