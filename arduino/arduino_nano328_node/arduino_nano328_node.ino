f6b165ef8591a2a54117efbc19d2c1d8b087b337
Greg Marcil <grmarcil@gmail.com>
2016-05-04 15:59:17 -0700

    Arduino Throttle Reverse (#6)
    Arduino Build Fixes

--- arduino/arduino_nano328_node/arduino_nano328_node.ino
+++ arduino/arduino_nano328_node/arduino_nano328_node.ino
@@ -1,24 +1,24 @@
 /* ---------------------------------------------------------------------------
-# Licensing Information: You are free to use or extend these projects for 
+# Licensing Information: You are free to use or extend these projects for
 # education or reserach purposes provided that (1) you retain this notice
-# and (2) you provide clear attribution to UC Berkeley, including a link 
+# and (2) you provide clear attribution to UC Berkeley, including a link
 # to http://barc-project.com
 #
 # Attibution Information: The barc project ROS code-base was developed
 # at UC Berkeley in the Model Predictive Control (MPC) lab by Jon Gonzales
-# (jon.gonzales@berkeley.edu)  Development of the web-server app Dator was 
-# based on an open source project by Bruce Wootton, with contributions from 
-# Kiet Lam (kiet.lam@berkeley.edu)   
+# (jon.gonzales@berkeley.edu)  Development of the web-server app Dator was
+# based on an open source project by Bruce Wootton, with contributions from
+# Kiet Lam (kiet.lam@berkeley.edu)
 # --------------------------------------------------------------------------- */
- 
- 
+
+
 /* ---------------------------------------------------------------------------
-WARNING: 
-* Be sure to have all ultrasound sensors plugged in, otherwise the pins may get stuck in 
+WARNING:
+* Be sure to have all ultrasound sensors plugged in, otherwise the pins may get stuck in
   some float voltage
 # --------------------------------------------------------------------------- */
 
- 
+
 // include libraries
 #include <ros.h>
 #include <barc/Ultrasound.h>
@@ -30,11 +30,11 @@ WARNING:
 // Number of encoder counts on tires
 // count tick on {FL, FR, BL, BR}
 // F = front, B = back, L = left, R = right
-volatile int FL_count = 0; 
+volatile int FL_count = 0;
 volatile int FR_count = 0;
 
 //encoder pins: pins 2,3 are hardware interrupts
-const int encPinA = 2; 
+const int encPinA = 2;
 const int encPinB = 3;
 
 // Actuator pins: 5,6
@@ -50,7 +50,7 @@ const int noAction = 0;
 
 // Actuator constraints (servo)
 // Not sure if constraints should be active on motor as well
-int d_theta_max = 50; 
+int d_theta_max = 50;
 int theta_center = 90;
 int motor_neutral = 90;
 int theta_max = theta_center + d_theta_max;
@@ -81,9 +81,9 @@ void messageCb(const barc::ECU& ecu){
   // deconstruct esc message
   motorCMD = saturateMotor( int(ecu.motor_pwm) );
   servoCMD = saturateServo( int(ecu.servo_pwm) );
-  
+
   // apply commands to motor and servo
-  motor.write( motorCMD );   
+  motor.write( motorCMD );
   steering.write(  servoCMD );
 }
 // ECU := Engine Control Unit
@@ -101,31 +101,31 @@ Maxbotix us_lt(17, Maxbotix::PW, Maxbotix::LV); // left
 ARDUINO INITIALIZATION
 **************************************************************************/
 void setup()
-{ 
+{
   // Set up encoder sensors
   pinMode(encPinA, INPUT_PULLUP);
   pinMode(encPinB, INPUT_PULLUP);
   attachInterrupt(0, FL_inc, CHANGE); // args = (digitalPintoInterrupt, ISR, mode), mode set = {LOW, CHANGE, RISING, FALLING}, pin 0 = INT0, which is pin D2
   attachInterrupt(1, FR_inc, CHANGE); //pin 1 = INT1, which is pin D3
- 
+
   // Set up actuators
   motor.attach(motorPin);
   steering.attach(servoPin);
-  
+
     // Start ROS node
   nh.initNode();
-  
+
   // Publish / Subscribe to topics
   nh.advertise(pub_ultrasound);
   nh.advertise(pub_encoder);
   nh.subscribe(s);
-  
+
   // Arming ESC, 1 sec delay for arming and ROS
   motor.write(theta_center);
   steering.write(theta_center);
   delay(1000);
   t0 = millis();
- 
+
 }
 
 
@@ -133,20 +133,20 @@ void setup()
 ARDUINO MAIN lOOP
 **************************************************************************/
 void loop()
-{  
+{
     // compute time elapsed (in ms)
   dt = millis() - t0;
-  
+
   // publish measurements
   if (dt > 50) {
     // publish encodeer measurement
-    
+
     encoder.FL = FL_count;
     encoder.FR = FR_count;
     encoder.BL = 0;
     encoder.BR = 0;
     pub_encoder.publish(&encoder);
-    
+
     // publish ultra-sound measurement
     /*
     ultrasound.front = us_fr.getRange();
@@ -157,7 +157,7 @@ void loop()
     pub_ultrasound.publish(&ultrasound);
     t0 = millis();
   }
-  
+
   nh.spinOnce();
 }
 
@@ -171,29 +171,29 @@ void FR_inc() { FR_count++; }
 /**************************************************************************
 SATURATE MOTOR AND SERVO COMMANDS
 **************************************************************************/
-int saturateMotor(int x) 
+int saturateMotor(int x)
 {
   if (x  == noAction ){ return motor_neutral; }
-  
-  if (x  >  motor_max) { 
-    x = motor_max; 
-  } 
+
+  if (x  >  motor_max) {
+    x = motor_max;
+  }
   else if (x < motor_min) {
     x = motor_min;
   }
   return x;
 }
 
-int saturateServo(int x) 
+int saturateServo(int x)
 {
-  
+
   if (x  == noAction ){
     return theta_center;
   }
-  
+
   if (x  >  theta_max) {
-    x = theta_max; 
-  } 
+    x = theta_max;
+  }
   else if (x < theta_min) {
     x = theta_min;
   }
