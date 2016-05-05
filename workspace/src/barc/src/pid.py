b2b8111d02c8520448729aac7232dc3e3bb421e3
Greg Marcil <grmarcil@gmail.com>
2016-05-05 01:33:27 -0700

    Convert pid.py to unix from dos format

--- workspace/src/barc/src/pid.py
+++ workspace/src/barc/src/pid.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 # ---------------------------------------------------------------------------
-# Licensing Information: You are free to use or extend these projects for 
+# Licensing Information: You are free to use or extend these projects for
 # education or reserach purposes provided that (1) you retain this notice
-# and (2) you provide clear attribution to UC Berkeley, including a link 
+# and (2) you provide clear attribution to UC Berkeley, including a link
 # to http://barc-project.com
 #
 # Attibution Information: The barc project ROS code-base was developed
 # at UC Berkeley in the Model Predictive Control (MPC) lab by Jon Gonzales
 # (jon.gonzales@berkeley.edu). The cloud services integation with ROS was developed
-# by Kiet Lam  (kiet.lam@berkeley.edu). The web-server app Dator was 
+# by Kiet Lam  (kiet.lam@berkeley.edu). The web-server app Dator was
 # based on an open source project by Bruce Wootton
 # ---------------------------------------------------------------------------
 
@@ -21,7 +21,7 @@ class PID:
 
         self.set_point      = 0             # reference point
         self.e              = 0
-        
+
         self.e_int          = 0
         self.int_e_max      = Integrator_max
         self.int_e_min      = Integrator_min
@@ -29,13 +29,13 @@ class PID:
     def update(self,current_value, dt):
         e_t         = self.set_point - current_value
         de_t        = ( e_t - self.e)/dt
-        
+
         self.e_int  = self.e_int + e_t * dt
         if self.e_int > self.int_e_max:
             self.e_int = self.int_e_max
         elif self.e_int < self.int_e_min:
             self.e_int = self.int_e_min
-            
+
         P_val  = self.Kp * e_t
         I_val  = self.Ki * self.e_int
         D_val  = self.Kd * de_t
@@ -63,27 +63,27 @@ class PID:
 
     def getError(self):
         return self.e
-  
+
 #%% Example function
 def fx(x, u, dt):
     x_next = x +  (3*x + u) * dt
     return x_next
-  
+
 #%% Test script to ensure program is functioning properly
 if __name__ == "__main__":
-    
+
     from numpy import zeros
     import matplotlib.pyplot as plt
-    
+
     n       = 200
     x       = zeros(n)
     x[0]    = 20
     pid     = PID(P=3.7, I=5, D=0.5)
     dt = 0.1
-    
+
     for i in range(n-1):
         u       = pid.update(x[i], dt)
         x[i+1] = fx(x[i],u, dt)
-         
+
     plt.plot(x)
     plt.show()
