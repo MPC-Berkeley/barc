a1d7436bd877b382909814e02db8f16fb068705a
Greg Marcil <grmarcil@gmail.com>
2016-05-19 16:49:37 -0700

    Add script to install EnableInterrupt arduino library

---
+++ scripts/install_enable_interrupt.sh
@@ -0,0 +1,4 @@
+cd ~/sketchbook/libraries
+wget -O enableinterrupt.zip "https://bintray.com/greygnome/generic/download_file?file_path=enableinterrupt-0.9.5.zip"
+unzip enableinterrupt.zip
+rm enableinterrupt.zip
