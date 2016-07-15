e3fee0cc77bf9721f5e5d79c8e6c3200dc2a6013
Jon Gonzales <jmgdrumin@gmail.com>
2016-07-15 16:27:10 -0700

    pid.h

---
+++ workspace/src/barc/src/pid.h
@@ -0,0 +1,24 @@
+#ifndef _PID_H_
+#define _PID_H_
+
+class PIDImpl;
+class PID
+{
+    public:
+        // Kp -  proportional gain
+        // Ki -  Integral gain
+        // Kd -  derivative gain
+        // dt -  loop interval time
+        // max - maximum value of manipulated variable
+        // min - minimum value of manipulated variable
+        PID( double dt, double max, double min, double Kp, double Kd, double Ki );
+
+        // Returns the manipulated variable given a setpoint and current process value
+        double calculate( double setpoint, double pv );
+        ~PID();
+
+    private:
+        PIDImpl *pimpl;
+};
+
+#endif
