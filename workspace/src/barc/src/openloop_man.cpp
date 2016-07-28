1002d0761793fe0a3a9bf27ec8595a1f1b51eec3
Jon Gonzales <jmgdrumin@gmail.com>
2016-07-27 19:34:38 -0700

    added six state msg file, and state estimation node for  6dof drift controller

---
+++ workspace/src/barc/src/openloop_man.cpp
@@ -0,0 +1,179 @@
+#include "ros/ros.h"
+#include"std_msgs/String.h"
+#include "geometry_msgs/Vector3.h"
+#include "data_service/TimeData.h"
+#include "nav_msgs/Odometry.h"
+#include "barc/ECU.h"
+#include <iostream>
+#include <fstream>
+#include "pid.h"
+#include <ctime>
+
+ros::Duration t ;
+ros::Time t0 ;
+int  n_FL, n_FR;
+int read_yaw0 = 0;
+float odom_vx,odom_vy,yaw0;
+float odom_wz,odom_x,odom_y;
+float odom_P;
+float vx_est,vy_est,r_est,beta_est;
+int activate_LQR = 0;
+float P[3][3] = {{2.8678,-0.7167,-0.98},{-0.7167,4.4277,-0.0443},{-0.98,-0.0443,1.4411}};
+float K[2][3] = {{-1.1052,-1.9455,0.5623},{2.0218,-2.2139,-1.1233}};
+float resual_ref=0.1721*5;
+float vx_ref = 1,beta_ref = -0.50,r_ref = 1.76;
+float F_yf_ref = 1.7153,F_xr_ref = 1.4377;
+float m = 1.98,mu = 0.234, a0 =0.1308, Ff=0.1711,L_a = 0.125;
+float B = 7.4,C=1.25;
+float pi = 3.1415926;
+int motorCMD_LQR,servoCMD_LQR;
+int max_steer,min_steer,max_motor,min_motor;
+
+float vx,vy,yr,X,Y,yaw;
+
+void enc_Callback(const geometry_msgs::Vector3  msg)
+{
+  n_FL = msg.x;
+  n_FR = msg.y;
+}
+
+void imu_Callback(const data_service::TimeData  msg)
+{
+  if(read_yaw0 == 0)
+  {
+    yaw0 = msg.value[2];
+    read_yaw0 = 1;
+  }
+  else
+  {
+  yr = msg.value[8];
+  yaw = msg.value[2];
+  }
+}
+void state_Callback(const geometry_msgs::Vector3 msg)
+{
+  vx_est = msg.x;
+  vy_est = msg.y;
+  r_est= msg.z;
+  //beta_est =atan(vy_est/vx_est);
+}
+
+void odom_Callback(const nav_msgs::Odometry  msg)
+ {
+  float temp = msg.twist.twist.linear.x/odom_P;
+  if (abs(temp - odom_vx) > 1) // donot updat when the state change large
+  {
+  }
+  else{
+  odom_vx = msg.twist.twist.linear.x/odom_P;
+  odom_vy = msg.twist.twist.linear.y/odom_P;
+  odom_wz = msg.twist.twist.angular.z/odom_P;
+  odom_x = msg.pose.pose.position.x/odom_P;
+  odom_y = msg.pose.pose.position.y/odom_P;
+  }
+}
+
+void gps_Callback(const geometry_msgs::Vector3  msg)
+{
+  X = msg.x;
+  Y = msg.y;
+}
+int main(int argc, char **argv)
+{
+ 
+  ros::init(argc, argv, "LQR_odo");
+  ros::NodeHandle enc;
+  ros::NodeHandle imu;
+  ros::NodeHandle odom;
+  ros::NodeHandle state_est;
+  ros::NodeHandle command;
+  ros::NodeHandle gps;
+  ros::NodeHandle param;
+
+ // ros::Subscriber enc_sub = enc.subscribe("enc_data", 1000, enc_Callback);
+  //ros::Subscriber imu_sub = imu.subscribe("imu_data", 1000, imu_Callback);
+  //ros::Subscriber odom_sub = odom.subscribe("mono_odometer/odometry", 1000, odom_Callback);
+  //ros::Subscriber state_sub = state_est.subscribe("state_estimate",100,state_Callback);
+  ros::Subscriber gps_sub = state_est.subscribe("indoor_gps",100,gps_Callback);
+  ros::Publisher com_pub = command.advertise<barc::ECU>("ecu",100);
+  float d_f,F_xR;
+  barc::ECU ecu;
+  ros::Rate r(50); // 50 Hz
+  t0 = ros::Time::now();
+
+
+  float t_hold,t_straight,t_turn,t_counter,t_recover;
+  float F_xR_straight,F_xR_turn,F_xR_counter,F_xR_recover;
+  float d_f_turn,d_f_counter;
+  param.getParam("/openloop_man/t_hold",t_hold) ;// time duration of stay still
+  param.getParam("/openloop_man/t_straight",t_straight) ;// time duration of go straight 
+  param.getParam("/openloop_man/t_turn",t_turn) ;// time duration of turning
+  param.getParam("/openloop_man/t_counter",t_counter) ;// time duration of counter-steer
+  param.getParam("/openloop_man/t_recover",t_recover) ;// time duration of go back to straight
+  param.getParam("/openloop_man/F_xR_straight",F_xR_straight) ;// rear wheel force to go straight
+  param.getParam("/openloop_man/F_xR_turn",F_xR_turn) ;// rear wheel force to turn
+  param.getParam("/openloop_man/F_xR_counter",F_xR_counter) ;// real wheel force when introducing counter-steering
+  param.getParam("/openloop_man/F_xR_recover",F_xR_recover) ;// real wheel force when going back to straight
+  param.getParam("/openloop_man/d_f_turn",d_f_turn) ;// steering angle when introducing steering
+  param.getParam("/openloop_man/d_f_counter",d_f_counter) ;// steering angle when introducing counter-steering
+
+
+  time_t time_now;
+  time(&time_now);
+  std::string s = ctime(&time_now); 
+  std::ofstream Data (("/home/odroid/Data/Drift_corner/"+s+".csv").c_str());
+  Data << "t,vx,vy,yawrate,X,Y,yaw,steeing,F_xR"<<std::endl;
+  while(ros::ok())
+  {
+
+    ros::spinOnce();
+    t = ros::Time::now()-t0;
+    if( t.toSec() < t_hold)  // stay still
+    {
+      F_xR = 0;
+      d_f = 0;
+    }
+    else if (t.toSec() < t_hold + t_straight) // go straight 
+    {
+      F_xR = F_xR_straight;
+      d_f = 0;
+
+    }
+    else if (t.toSec() < t_hold + t_straight + t_turn)  // make a turn 
+    {
+      F_xR = F_xR_turn;
+      d_f = d_f_turn;
+
+    }
+    else if (t.toSec() < t_hold + t_straight + t_turn + t_counter)      //introduce counter_steering 
+    {
+    F_xR = F_xR_counter;
+    d_f = d_f_counter;
+    }
+    else if (t.toSec() < t_hold + t_straight + t_turn + t_counter + t_recover)//         go back to straight
+    {
+      F_xR = F_xR_recover;
+      d_f = 0;
+    }
+    else
+    {
+      F_xR = 0;
+      d_f = 0;
+    }
+
+    Data << t << "," << vx << "," << vy << "," << std::endl;
+    //std::cout << "d_f :" << d_f <<std::endl;
+    //std::cout << "F_xR :" << F_xR <<std::endl;
+
+
+    
+
+    ecu.motor = F_xR;
+    ecu.servo = d_f*pi/180; //degree -> rad
+    com_pub.publish(ecu);
+    
+    r.sleep();
+  }
+  return 0;
+   
+}
---
+++ workspace/src/barc/src/state_estimation_DynBkMdl_6s.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+
+# ---------------------------------------------------------------------------
+# Licensing Information: You are free to use or extend these projects for 
+# education or reserach purposes provided that (1) you retain this notice
+# and (2) you provide clear attribution to UC Berkeley, including a link 
+# to http://barc-project.com
+#
+# Attibution Information: The barc project ROS code-base was developed
+# at UC Berkeley in the Model Predictive Control (MPC) lab by Jon Gonzales
+# (jon.gonzales@berkeley.edu). The cloud services integation with ROS was developed
+# by Kiet Lam  (kiet.lam@berkeley.edu). The web-server app Dator was 
+# based on an open source project by Bruce Wootton
+# ---------------------------------------------------------------------------
+
+import rospy
+import time
+import os
+from barc.msg import ECU, Encoder
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Vector3
+from numpy import pi, cos, sin, eye, array, zeros
+from observers import kinematicLuembergerObserver, ekf
+from system_models import f_6s, h_6s
+from tf import transformations
+from numpy import unwrap
+
+# input variables
+d_f 	    = 0
+FxR         = 0
+
+# raw measurement variables from IMU
+yaw_prev = 0
+(roll, pitch, yaw, a_x, a_y, a_z, w_x, w_y, w_z) = zeros(9)
+
+
+# raw measurement variables from indoor GPS
+(X_gps, Y_gps) = zeros(2)
+
+# raw measurement variables from optic flow
+
+(v_x_optic,v_y_optic) = zeros(2)
+# from encoder
+v_x_enc 	= 0
+t0 	        = time.time()
+n_FL	    = 0                     # counts in the front left tire
+n_FR 	    = 0                     # counts in the front right tire
+n_FL_prev 	= 0
+n_FR_prev 	= 0
+r_tire 		= 0.04                  # radius from tire center to perimeter along magnets [m]
+dx_qrt 	    = 2.0*pi*r_tire/4.0     # distance along quarter tire edge
+
+# ecu command update
+def ecu_callback(data):
+    global FxR, d_f
+    FxR         = data.motor        # input motor force [N]
+    d_f         = data.servo        # input steering angle [rad]
+
+# imu measurement update
+def imu_callback(data):
+    # units: [rad] and [rad/s]
+    global roll, pitch, yaw, a_x, a_y, a_z, w_x, w_y, w_z
+    global yaw_prev
+    
+    # get orientation from quaternion data, and convert to roll, pitch, yaw
+    # extract angular velocity and linear acceleration data
+    ori  = data.orientation
+    quaternion  = (ori.x, ori.y, ori.z, ori.w)
+    (roll, pitch, yaw) = transformations.euler_from_quaternion(quaternion)
+    yaw         = unwrap(array([yaw_prev, yaw]), discont = pi)[1]
+    yaw_prev    = yaw
+    
+    # extract angular velocity and linear acceleration data
+    w_x = data.angular_velocity.x
+    w_y = data.angular_velocity.y
+    w_z = data.angular_velocity.z
+    a_x = data.linear_acceleration.x
+    a_y = data.linear_acceleration.y
+    a_z = data.linear_acceleration.z
+    rospy.loginfo("I heard IMU")
+
+# encoder measurement update
+def enc_callback(data):
+	global v_x_enc, d_f, t0
+	global n_FL, n_FR, n_FL_prev, n_FR_prev
+
+	n_FL = data.FL
+	n_FR = data.FR
+
+	# compute time elapsed
+	tf = time.time()
+	dt = tf - t0
+	
+	# if enough time elapse has elapsed, estimate v_x
+	dt_min = 0.20
+	if dt >= dt_min:
+		# compute speed :  speed = distance / time
+		v_FL = float(n_FL- n_FL_prev)*dx_qrt/dt
+		v_FR = float(n_FR- n_FR_prev)*dx_qrt/dt
+
+		# update encoder v_x, v_y measurements
+		# only valid for small slip angles, still valid for drift?
+		v_x_enc 	= (v_FL + v_FR)/2.0*cos(d_f)
+
+		# update old data
+		n_FL_prev   = n_FL
+		n_FR_prev   = n_FR
+		t0 	        = time.time()
+
+# indoor gps measurement update
+def gps_callback(data):
+    global X_gps, Y_gps
+    X_gps = data.x
+    Y_gps = data.y
+    rospy.loginfo("I heard indoor gps")
+
+# indoor gps measurement update
+def optic_callback(data):
+    global v_x_optic,v_y_optic
+    v_x_optic = data.x
+    v_y_optic = data.y
+    rospy.loginfo("I heard optic flow")
+
+# state estimation node
+def state_estimation():
+	# initialize node
+    rospy.init_node('state_estimation', anonymous=True)
+
+    # topic subscriptions / publications
+    rospy.Subscriber('/imu/data', Imu, imu_callback)
+    rospy.Subscriber('/encoder', Encoder, enc_callback)
+    rospy.Subscriber('/ecu', ECU, ecu_callback)
+    rospy.Subscriber('/indoor_gps',Vector3 , gps_callback)
+    rospy.Subscriber('/camera/vel_est',Vector3 , optic_callback)
+    state_pub 	= rospy.Publisher('state_estimate', Vector3, queue_size = 10)
+
+	# get vehicle dimension parameters
+    L_a = rospy.get_param("/L_a")       # distance from CoG to front axel
+    L_b = rospy.get_param("/L_b")       # distance from CoG to rear axel
+    m   = rospy.get_param("/m")         # mass of vehicle
+    I_z = rospy.get_param("/I_z")       # moment of inertia about z-axis
+    vhMdl   = (L_a, L_b, m, I_z)
+
+    # get encoder parameters
+    dt_vx   = rospy.get_param("/state_estimation/dt_v_enc")     # time interval to compute v_x
+
+    # get tire model
+    B   = rospy.get_param("/tire_model/B")
+    C   = rospy.get_param("/tire_model/C")
+    mu  = rospy.get_param("/tire_model/mu")
+    TrMdl = ([B,C,mu],[B,C,mu])
+
+    # get external force model
+    a0  = rospy.get_param("/air_drag_coeff")
+    Ff  = rospy.get_param("/friction")
+
+    # get EKF observer properties
+    q_std   = rospy.get_param("/state_estimation/q_std")             # std of process noise
+    r_std   = rospy.get_param("/state_estimation/r_std")             # std of measurementnoise
+    v_x_min     = rospy.get_param("/state_estimation/v_x_min")  # minimum velociy before using EKF
+
+	# set node rate
+    loop_rate 	= 50
+    dt 		    = 1.0 / loop_rate
+    rate 		= rospy.Rate(loop_rate)
+    t0 			= time.time()
+
+    # estimation variables for Luemberger observer
+    z_EKF       = array([1.0, 0.0, 0.0])
+
+    # estimation variables for EKF
+    P           = eye(6)                # initial dynamics coveriance matrix
+    Q           = (q_std**2)*eye(6)     # process noise coveriance matrix
+    R           = (r_std**2)*eye(5)     # measurement noise coveriance matrix
+
+    while not rospy.is_shutdown():
+
+		# publish state estimate
+        (v_x, v_y, r) = z_EKF           # note, r = EKF estimate yaw rate
+
+        # publish information
+        state_pub.publish( Vector3(v_x, v_y, r) )
+
+        # apply EKF
+        if v_x_enc > v_x_min:
+            # get measurement
+            y = array([v_x_enc, w_z])
+
+            # define input
+            u       = array([ d_f, FxR ])
+
+            # build extra arguments for non-linear function
+            F_ext = array([ a0, Ff ]) 
+            args = (u, vhMdl, TrMdl, F_ext, dt) 
+
+            # apply EKF and get each state estimate
+            (z_EKF,P) = ekf(f_6s, z_EKF, P, h_6s, y, Q, R, args )
+
+        else:
+            z_EKF[0] = float(v_x_enc)
+            z_EKF[2] = float(w_z)
+        
+		# wait
+        rospy.loginfo(" X: %f", X_gps)
+        rospy.loginfo(" Y: %f", Y_gps)
+        rospy.loginfo(" yaw angle: %f", yaw)
+        rospy.loginfo(" yaw rate: %f", w_z)
+        rospy.loginfo(" vx: %f", v_x_optic)
+        rospy.loginfo(" vy: %f", v_y_optic)
+        rate.sleep()
+
+if __name__ == '__main__':
+	try:
+	   state_estimation()
+	except rospy.ROSInterruptException:
+		pass
