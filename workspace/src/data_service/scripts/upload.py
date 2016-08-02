39bd9d1a0c2e0b4fb9024120aac3bf7dde37fdd1
Kiet Lam <kietdlam@gmail.com>
2016-08-02 10:57:58 -0700

    Updated upload code.

--- workspace/src/data_service/scripts/upload.py
+++ workspace/src/data_service/scripts/upload.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 # ---------------------------------------------------------------------------
-# Licensing Information: You are free to use or extend these projects for 
+# Licensing Information: You are free to use or extend these projects for
 # education or reserach purposes provided that (1) you retain this notice
-# and (2) you provide clear attribution to UC Berkeley, including a link 
+# and (2) you provide clear attribution to UC Berkeley, including a link
 # to http://barc-project.com
 #
 # Attibution Information: The barc project ROS code-base was developed
 # at UC Berkeley in the Model Predictive Control (MPC) lab by Jon Gonzales
 # (jon.gonzales@berkeley.edu). The cloud services integation with ROS was developed
-# by Kiet Lam  (kiet.lam@berkeley.edu). The web-server app Dator was 
+# by Kiet Lam  (kiet.lam@berkeley.edu). The web-server app Dator was
 # based on an open source project by Bruce Wootton
 # ---------------------------------------------------------------------------
 
@@ -32,17 +32,56 @@ application = get_wsgi_application()
 
 from data_api.models import *
 
+import boto3
+
 CLOUD_CONFIG_LOCATION = '/home/odroid/cloud.cfg'
+
+
+S3_VIDEOS_BUCKET = 'datorvideos'
+
+rosbag_dir = os.path.expanduser("~") + '/rosbag'
+video_dir = os.path.expanduser("~") + '/video'
+
 
 if __name__ == '__main__':
     configurator = init_configurator(CLOUD_CONFIG_LOCATION)
     data_connection = DataConnection(configurator)
 
+    s3_client = boto3.client('s3')
+    s3 = boto3.resource('s3')
+
     for sig in Signal.objects.all():
         try:
             experiment = data_connection.get_or_create_experiment(sig.experiment.name)
+
+            for setting in sig.experiment.setting_set.all():
+                setting_remote = data_connection.get_or_create_setting(setting.key, experiment)
+
+                #TODO: Check if AWS S3 token exists
+                if setting.key == 'video':
+                    if setting.value.startswith(video_dir):
+                        key_name = '%s_%s.avi' % (os.environ['TEAM_NAME'], sig.experiment.name)
+
+                        bucket = s3.Bucket(S3_VIDEOS_BUCKET)
+                        bucket.Acl().put(ACL='public-read')
+
+                        obj = s3.Object(S3_VIDEOS_BUCKET, key_name)
+                        print 'Uploading video'
+                        video_path = '%s/%s.avi' %(video_dir, sig.experiment.name)
+                        obj.put(Body=open(video_path, 'rb'))
+                        obj.Acl().put(ACL='public-read')
+
+                        print 'Finished uploading video'
+                        url = '{}/{}/{}'.format(s3_client.meta.endpoint_url,
+                                                S3_VIDEOS_BUCKET, key_name)
+                        setting.value = url
+                        setting.save()
+
+                        os.remove(video_path)
+
+                data_connection.write_setting(setting.value, setting_remote['id'])
+
             signal = data_connection.get_or_create_signal(sig.name, experiment)
-            print signal
 
             try:
                 lst = LocalSignalTag.objects.filter(signal__name=sig.name, signal__experiment__name=sig.experiment.name)[0]
