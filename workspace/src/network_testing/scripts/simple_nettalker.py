c767245acb8b208c85e4d7702aa9e300c352251c
Jon Gonzales <jmgdrumin@gmail.com>
2016-11-21 13:34:16 -0800

    Added tests

---
+++ workspace/src/network_testing/scripts/simple_nettalker.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+from barc.msg import ECU
+from std.msgs.msg import String
+
+
+def lead():
+    sig = rospy.Publisher('signal', barc/ECU, queue_size=10)
+    ecu = rospy.Publisher('/ecu', barc/ECU, queue_size=10)
+
+    rospy.init_node('Car_2')
+
+    r = rospy.rate(1)
+
+    while not rospy.is_shutdown():
+        cmd = ECU(0, 30)
+        ecu.publish(cmd)
+        sig.publish(cmd)
+
+        r.sleep()
+
+        cmd = ECU(0, -30)
+        ecu.publish(cmd)
+        sig.publish(cmd)
+
+        r.sleep()
+
+if __name__ == '__main__':
+    try:
+        lead()
+    except rospy.ROSInterruptException:
+        pass
+    
+
+
+
