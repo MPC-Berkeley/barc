9435956fef839a2a866273e18cfa311c6e8ca3c2
Maximilian Brunner <mabrunne@student.ethz.ch>
2016-12-02 11:48:53 -0800

    Added odroid_setup script

---
+++ odroid_setup.sh
@@ -0,0 +1,5 @@
+# Change these IP addresses according to your computer and odroid addresses and run the script before starting experiments
+# In terminal:
+# $source odroid_setup.sh
+export ROS_MASTER_URI=http://192.168.100.152:11311
+export ROS_IP=192.168.100.152
