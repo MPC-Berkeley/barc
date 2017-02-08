50a1aee07dd1414d925cd585d165ab5951caf0f7
Jon Gonzales <jon.gonzales@berkeley.edu>
2017-02-08 15:13:53 -0800

    updating scripts with master branch

--- scripts/rebuild_system.sh
+++ scripts/rebuild_system.sh
@@ -5,10 +5,18 @@ catkin_make clean
 catkin_make
 
 # configure ROS environment
-source /home/odroid/barc/workspace/devel/setup.bash
+source $HOME/barc/workspace/devel/setup.bash
+
+# create arduino upload-folder
+# re-initialize space
+cd $HOME/barc/arduino/
+mkdir -p .arduino_nano328_node
+cd .arduino_nano328_node
+ano init -t blink; rm src/sketch.ino
+cp $HOME/barc/arduino/arduino_nano328_node/arduino_nano328_node.ino src/
 
 # rebuild arduino libraries
-cd ~/sketchbook/libraries
+cd $HOME/sketchbook/libraries
 rm -rf ros_lib
 rosrun rosserial_arduino make_libraries.py .
 cd
@@ -22,8 +30,8 @@ fi
 
 # flash arduino if connected
 if [ -e /dev/ttyUSB0 ]; then
-    cd ~/barc/arduino/.arduino_nano328_node
-    cp ~/barc/arduino/arduino_nano328_node/arduino_nano328_node.ino src/
+    cd $HOME/barc/arduino/.arduino_nano328_node
+    cp $HOME/barc/arduino/arduino_nano328_node/arduino_nano328_node.ino src/
     ano clean
     ano build -m nano328
     ano upload -m nano328 -p /dev/ttyUSB0
