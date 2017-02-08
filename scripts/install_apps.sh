5b50f6ab3a7161bd714d03a6c04bbac3704aac59
Jon Gonzales <jon.gonzales@berkeley.edu>
2017-02-08 14:43:52 -0800

    merged two install scripts into one

--- scripts/install_apps.sh
+++ scripts/install_apps.sh
@@ -27,6 +27,11 @@ if ! hash ano 2>/dev/null; then
     cd;
 fi
 
+# install EnableInterrupt library for arduino
+cd ~/sketchbook/libraries
+wget -O enableinterrupt.zip "https://bintray.com/greygnome/generic/download_file?file_path=enableinterrupt-0.9.5.zip"
+unzip enableinterrupt.zip
+rm enableinterrupt.zip
 
 # install monokai colors for vim
 cd
