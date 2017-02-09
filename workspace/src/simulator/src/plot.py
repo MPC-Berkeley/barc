099849170b6a8832d48e3af64b6155b2a6956f2b
Jon Gonzales <jon.gonzales@berkeley.edu>
2017-02-09 01:07:41 -0800

    updated simulator package with latest code from simulator branch

--- workspace/src/simulator/src/plot.py
+++ workspace/src/simulator/src/plot.py
@@ -1,26 +1,25 @@
 import rosbag
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-bag = rosbag.Bag('2016-12-03-10-27-10.bag')
-
+bag = rosbag.Bag(os.path.expanduser("~/rosbag/2017-01-31-21-27-32.bag"))
 
 
 topics = bag.get_type_and_topic_info()[1].keys()
 types = []
 for i in range(0,len(bag.get_type_and_topic_info()[1].values())):
   types.append(bag.get_type_and_topic_info()[1].values()[i][0])
+
+  print bag.get_type_and_topic_info()[1].values()[i][0]
+
   if bag.get_type_and_topic_info()[1].values()[i][0] == 'barc/ECU':
     dimEcu = bag.get_type_and_topic_info()[1].values()[i][1]
     print "Dim ECU mgs:", dimEcu
 
-  if bag.get_type_and_topic_info()[1].values()[i][0] == 'marvelmind_nav/hedge_pos':
+  if bag.get_type_and_topic_info()[1].values()[i][0] == 'simulator/Z_DynBkMdl':
     dimxy = bag.get_type_and_topic_info()[1].values()[i][1]
     print "Dim hedge_pos msg:", dimxy
-
-  if bag.get_type_and_topic_info()[1].values()[i][0] == 'barc/Vel_est':
-    dimVel = bag.get_type_and_topic_info()[1].values()[i][1]
-    print "Dim Vel_est msg:", dimVel
 
 print types
 
@@ -28,45 +27,46 @@ print types
 x_raw = np.zeros((dimxy, 1))
 y_raw = np.zeros((dimxy, 1))
 
-v_raw = np.zeros((dimVel, 1))
+v_raw = np.zeros((dimxy, 1))
 
 acc   = np.zeros((dimEcu, 1))
 d_f   = np.zeros((dimEcu, 1))
 time  = np.zeros((dimEcu, 1))
 
 counter = 0
-for counter, (topic, msg, t) in enumerate( bag.read_messages(topics=['/hedge_pos']) ) :  
-  x_raw[counter] = msg.x_m
-  y_raw[counter] = msg.y_m
-  counter =+ 1
-
-counter = 0
-for counter, (topic, msg, t) in enumerate( bag.read_messages(topics=['/vel_est']) ) :  
-  v_raw[counter] = msg.vel_est
-  counter =+ 1
+for counter, (topic, msg, t) in enumerate( bag.read_messages(topics=['/z_vhcl']) ) :  
+  x_raw[counter] = msg.x
+  y_raw[counter] = msg.y
+  v_raw[counter] = np.sqrt( np.power(msg.v_x,2) + np.power(msg.v_x,2) )
 
 
 counter = 0
 for (topic, msg, t) in bag.read_messages(topics=['/ecu']) :  
-  acc[counter] = msg.motor
-  d_f[counter] = msg.servo
+  acc[counter]  = msg.motor
+  d_f[counter]  = msg.servo
   counter = counter + 1
 
 
-print counter
+# print counter
 
 plt.figure(1)
-plt.plot(x_raw, y_raw,'-o')
+plt.plot(x_raw, y_raw)
+plt.ylabel('Position along Y-axis [m]')
+plt.xlabel('Position along X-axis [m]')
 
 plt.figure(2)
 plt.plot(v_raw)
+plt.ylabel('Velocity [m/s]')
+
 
 plt.figure(3)
-plt.plot(d_f, '-o')
+plt.plot(d_f)
+plt.ylabel('Steering [rad]')
 
 
 plt.figure(4)
-plt.plot(acc, '-o')
+plt.plot(acc)
+plt.ylabel('Acceleration [m/s^2]')
 
 plt.show()
 
