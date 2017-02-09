e5aeb9146f31ee7f572247482bea1d1947aa25d6
Jon Gonzales <jon.gonzales@berkeley.edu>
2017-02-08 17:29:21 -0800

    added some comments on each command in script file

--- env_loader_pc.sh
+++ env_loader_pc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source /opt/ros/kinetic/setup.bash
-source /home/ugo/GitHub/barc/workspace/devel/setup.bash
-export PATH=$PATH:/home/ugo/julia/bin/julia
-export ROS_IP=192.168.100.72
-export ROS_MASTER_URI=http://192.168.100.72:11311
-exec "$@"
+source /opt/ros/kinetic/setup.bash                  # set-up system-wide ROS gloval environment variables (kinetic version)
+source $HOME/barc/workspace/devel/setup.bash        # set-up ROS environment variables within workspace
+export PATH=$PATH:~julia-0.5/bin/                   # set-up link to Julia programming language
+export ROS_IP=192.168.100.72                        # set IP address of this machine (laptop)
+export ROS_MASTER_URI=http://192.168.100.72:11311   # set IP address of master machine 
+exec "$@"                                           # redirect input arguments
