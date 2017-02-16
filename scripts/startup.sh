9b6e6abefdfac2ccf7e94f7a522168a5a2953f76
Jon Gonzales <jon.gonzales@berkeley.edu>
2017-02-16 13:35:15 -0800

    updated start script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -25,3 +25,10 @@ alias tmux='tmux -2'
 
 # set configuration script for vim text editor
 cp ~/barc/scripts/vimrc ~/.vimrc
+
+
+# added git branch listing (michael)
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
