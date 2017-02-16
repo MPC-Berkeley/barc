1509688fd9179689083484ae545f5744a4bcbd68
Jon Gonzales <jon.gonzales@berkeley.edu>
2017-02-16 13:30:20 -0800

    updated IP address of pc to reflect address on network

--- scripts/remote-machines/env_loader_odroid.sh
+++ scripts/remote-machines/env_loader_odroid.sh
@@ -4,5 +4,5 @@ source /opt/ros/indigo/setup.bash                   # set up system-wide global 
 source $HOME/barc/workspace/devel/setup.bash        # set up ROS environment variables within workspace
 #export PATH=$PATH:~/julia                           # add Julia programming language to path
 export ROS_IP=192.168.100.100                       # set IP address of this machine (micro-controller)
-export ROS_MASTER_URI=http://192.168.100.101:11311  # set IP address of master machine
+export ROS_MASTER_URI=http://192.168.100.102:11311  # set IP address of master machine
 exec "$@"
