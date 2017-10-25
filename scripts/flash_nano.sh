6f1c67f8cb1fe8d06b57cece2f235eb0f75dddc7
Jon Gonzales <jmgdrumin@gmail.com>
2017-10-24 19:59:54 -0700

    fixed link in flash_nano utility

--- scripts/flash_nano.sh
+++ scripts/flash_nano.sh
@@ -2,8 +2,11 @@ if [ ! -d ~/barc/arduino/.arduino_nano328_node ]; then
     mkdir -p ~/barc/arduino/.arduino_nano328_node/src
 fi
 
+if [ ! -L ~/barc/arduino/.arduino_nano328_node/lib ]; then
+    ln -s ~/sketchbook/libraries ~/barc/arduino/.arduino_nano328_node/lib
+fi
+
 cd ~/barc/arduino/.arduino_nano328_node 
-ln -s ~/sketchbook/libraries lib
 cp ../arduino_nano328_node/arduino_nano328_node.ino src/; 
 ano clean; ano build -m nano328; 
 ano upload -m nano328 -p /dev/ttyUSB0; 
