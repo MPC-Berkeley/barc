fb3fffb3e42a0a0b7dc282b481ed224f956d74f5
Jon Gonzales <jon.gonzales@berkeley.edu>
2017-12-07 17:37:11 -0800

    removed data service message

--- workspace/src/barc/src/LMPC_node.jl
+++ workspace/src/barc/src/LMPC_node.jl
@@ -2,11 +2,11 @@
 
 using RobotOS
 @rosimport barc.msg: ECU, pos_info
-@rosimport data_service.msg: TimeData
+#@rosimport data_service.msg: TimeData
 @rosimport geometry_msgs.msg: Vector3
 rostypegen()
 using barc.msg
-using data_service.msg
+#using data_service.msg
 using geometry_msgs.msg
 using JuMP
 using Ipopt
