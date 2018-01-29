42025d7797fd4f6074a80fffa7f8f5313e55a9b4
Jon Gonzales <jon.gonzales@berkeley.edu>
2018-01-29 21:57:56 +0000

    Added vino script for VNCViewer

---
+++ scripts/fix_vino.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get purge vino
+sudo apt-get install vino
+
+dconf write /org/gnome/desktop/remote-access/enabled true
+dconf write /org/gnome/desktop/remote-access/prompt-enabled false
+dconf write /org/gnome/desktop/remote-access/require-encryption false
+
+echo "[Desktop Entry]
+Type=Application
+Exec=/usr/lib/vino/vino-server
+Hidden=false
+X-MATE-Autostart-enabled=true
+Name[en]=Vino Server
+Name=Vino Server
+Comment[en]=VNC Remote Desktop Server
+Comment=VNC Remote Desktop Server" > ~/.config/autostart/vino-server.desktop
+
+echo "[SeatDefaults]
+autologin-user=odroid
+autologin-user-timeout=0" > /etc/lightdm/lightdm.conf
+
+sudo service lightdm restart
+#sudo reboot now
