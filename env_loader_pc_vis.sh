6554645a46940f3052ca74bbd5abcc25a928a6ed
Jon Gonzales <jon.gonzales@berkeley.edu>
2018-05-30 07:20:08 -0700

    Added and updated some env loaders

--- env_loader_pc_vis.sh
+++ env_loader_pc_vis.sh
@@ -2,7 +2,9 @@
 source /opt/ros/kinetic/setup.bash
 source /home/mpcubuntu/barc/workspace/devel/setup.bash
 #export PATH=$PATH:/home/ugo/julia/bin/julia
+# export ROS_IP=192.168.10.147
+# export ROS_MASTER_URI=http://192.168.10.74:11311
+# export DISPLAY=:0.0
 export ROS_IP=192.168.10.147
-export ROS_MASTER_URI=http://192.168.10.74:11311
-export DISPLAY=:0.0
+export ROS_MASTER_URI=http://192.168.10.147:11311
 exec "$@"
