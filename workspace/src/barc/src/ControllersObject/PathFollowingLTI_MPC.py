Fixing change of coordinates for rotation

workspace/src/barc/src/ControllersObject/PathFollowingLTI_MPC.py
@@ -234,7 +234,7 @@ def _buildMatCost(Controller):
     Mx = linalg.block_diag(*b)
     Qlane = Controller.Qlane
 
-    dR = np.array([40, 10])
+    dR = np.array([2 * 40, 10])
     c = [R + 2 * np.diag(dR)] * (N) # Need to add dR for the derivative input cost
 
     Mu = linalg.block_diag(*c)

workspace/src/barc/src/Tools/estimatorPlayBack.py
@@ -28,7 +28,7 @@ def main():
     # node initialization
     a_delay     = 0.0
     df_delay    = 0.0
-    loop_rate   = 50.0
+    loop_rate   = 100.0
    
     # Red
     Q_noVy = eye(8)
@@ -67,8 +67,8 @@ def main():
     Q_hs[4,4]  =  1.0 #1.0      # ax
     Q_ls[4,4]  =  1.0 #1.0      # ax
     
-    Q_hs[5,5]  =  1.0 #1.0      # ay 
-    Q_ls[5,5]  =  1.0 #1.0      # ay 
+    Q_hs[5,5]  =  100.0 #1.0      # ay 
+    Q_ls[5,5]  =  100.0 #1.0      # ay 
     
     Q_hs[6,6]  =  50.0 #10 + 80.0      # psi
     Q_ls[6,6]  =  10.0 #10 + 80.0      # psi
@@ -90,7 +90,7 @@ def main():
     R_hs[3,3]  = 10.0 #30 + 10.0      # ax 
     R_ls[3,3]  = 10.0 #30 + 10.0      # ax 
 
-    R_hs[4,4]  = 30.0 #40.0      # ay 
+    R_hs[4,4]  = 1.0 #40.0      # ay 
     R_ls[4,4]  = 10.0 #40.0      # ay 
 
     R_hs[5,5]  = 0.1 #5 * 5 * 2 * 10 * 0.1      # psiDot
@@ -235,46 +235,51 @@ def main():
     inp_psiDot_his      = npz_output["inp_psiDot_his"]
     inp_a_his           = npz_output["inp_a_his"]
     inp_df_his          = npz_output["inp_df_his"]
-
-
-    pathSave = os.path.join(homedir,"barc_data/estimator_imu.npz")
-    npz_imu = np.load(pathSave)
-    psiDot_his        = npz_imu["psiDot_his"]
-    roll_his          = npz_imu["roll_his"]
-    pitch_his         = npz_imu["pitch_his"]
-    yaw_his          = npz_imu["yaw_his"]
-    ax_his          = npz_imu["ax_his"]
-    ay_his          = npz_imu["ay_his"]
-
-    pathSave = os.path.join(homedir,"barc_data/estimator_gps.npz")
-    npz_gps = np.load(pathSave)
-    x_his         = npz_gps["x_his"]
-    y_his         = npz_gps["y_his"]
-
-    pathSave = os.path.join(homedir,"barc_data/estimator_enc.npz")
-    npz_enc = np.load(pathSave)
-    v_fl_his     = npz_enc["v_fl_his"]
-    v_fr_his     = npz_enc["v_fr_his"]
-    v_rl_his     = npz_enc["v_rl_his"]
-    v_rr_his     = npz_enc["v_rr_his"]
-
-    pathSave = os.path.join(homedir,"barc_data/estimator_ecu.npz")
-    npz_ecu = np.load(pathSave)
-    a_his         = npz_ecu["a_his"]
-    df_his        = npz_ecu["df_his"]
+    roll_his            = npz_output["roll_his"]
+    pitch_his           = npz_output["pitch_his"]
+    wx_his              = npz_output["wx_his"]
+    wy_his              = npz_output["wy_his"]
+    wz_his              = npz_output["wz_his"]
+    v_rl_his            = npz_output["v_rl_his"]
+    v_rr_his            = npz_output["v_rr_his"]
+    v_fl_his            = npz_output["v_fl_his"]
+    v_fr_his            = npz_output["v_fr_his"]
+    
 
     fig = plotTrack(map)
     plt.axis("equal")
-    plt.plot(x_his,y_his,color="blue")
-
-    plt.show()
+    plt.plot(inp_x_his,inp_y_his,color="blue")
 
     plt.figure("Encoder Input")
-    plt.plot(inp_v_meas_his, 'o-')
+    axes = plt.gca()
+    plt.plot(v_rl_his,'--sb', label="v_rl")
+    plt.plot(v_rr_his,'--ob', label="v_rr")
+    plt.plot(v_fl_his,'--sk', label="v_fl")
+    plt.plot(v_fr_his,'--ok', label="v_fr")
+    plt.plot(inp_v_meas_his,'--or', label="v_x")
+    axes.legend()
+    
+    plt.figure("IMU")
+    plt.subplot(211)
+    axes = plt.gca()
+    plt.plot(wx_his, '-o', label="wx")
+    plt.plot(wy_his, '-s', label="wy")
+    plt.plot(wz_his, '-*', label="wz")
+    plt.plot(inp_psiDot_his, '--o', label="psiDot")
+    plt.legend()
+
+    plt.subplot(212)
+    axes = plt.gca()
+    plt.plot(pitch_his, '-o', label="pitch")
+    plt.plot(roll_his, '-s', label="roll")
+    axes.legend()
+
     plt.show()
 
+
+
     flagHalfLap = False
-    plotDebug = True
+    plotDebug = False
 
 
     if plotDebug == True:

workspace/src/barc/src/controllerMain.py
@@ -60,13 +60,13 @@ def main():
 
     PickController = "LMPC"
     NumberOfLaps   = 30
-    vt = 1.2
+    vt = 1.3
     PathFollowingLaps = 2
     
     if mode == "simulations":
         PIDnoise = np.array([1.0, 1.0]) # noise on [Steering, Acceleration] 
     else:
-        PIDnoise = np.array([1.0, 1.0]) # noise on [Steering, Acceleration] 
+        PIDnoise = np.array([0.1, 10.0]) # noise on [Steering, Acceleration] 
 
     ControllerLap0, Controller,  OpenLoopData   = ControllerInitialization(PickController, NumberOfLaps, dt, vt, map, mode, PIDnoise)
                  
@@ -231,7 +231,7 @@ def main():
 
     # Save Data
     if saveData == True:
-        file_data = open(homedir+'/barc_data/'+'/ClosedLoopData'+PickController+'.obj', 'wb')
+        file_data = open(homedir+'/barc_data'+'/ClosedLoopData'+PickController+'.obj', 'wb')
         pickle.dump(ClosedLoopData, file_data)
         pickle.dump(Controller, file_data)
         pickle.dump(OpenLoopData, file_data)    
@@ -263,7 +263,10 @@ def ControllerInitialization(PickController, NumberOfLaps, dt, vt, map, mode, PI
         file_data = open(homedir+'/barc_data/'+'/ClosedLoopDataPID.obj', 'rb')
         ClosedLoopDataPID = pickle.load(file_data)
         file_data.close()
-        lamb = 0.0000001
+        if mode == "simulations":
+            lamb = 0.0000001
+        else:
+            lamb = 0.0000001    
         A, B, Error = Regression(ClosedLoopDataPID.x, ClosedLoopDataPID.u, lamb)
         print "A matrix: \n", A
         print "B matrix: \n", B      
@@ -357,8 +360,8 @@ class PID:
             Steering = - 0.5 * 2.0 * x0[5] - 2 * 0.5 * x0[3] - 0.001 * self.integral[0]
             Accelera = 0.5 * 1.5 * (vt - x0[0]) + 0.1 * self.integral[1]
         else:
-            Steering = - 0.5 * x0[5] - x0[3] - 0.01 * self.integral[0]
-            Accelera = 1.5 * (vt - x0[0]) + 0.01 * self.integral[1]
+            Steering = - 0.5 * x0[5] - x0[3] - 0.1 * self.integral[0]
+            Accelera = 1.5 * (vt - x0[0]) + 0.1 * self.integral[1]
 
         self.integral[0] = self.integral[0] +  0.1 * x0[5] + 0.1 * x0[3]
         self.integral[1] = self.integral[1] +  (vt - x0[0])

workspace/src/barc/src/stateEstimatorInputs.py
@@ -314,7 +314,7 @@ class Estimator(object):
 
         self.motor_his.append(ecu.a)
         self.servo_his.append(ecu.df)
-        u = [self.motor_his.pop(0), self.servo_his.pop(0), imu.psiDot, imu.ax, imu.ay]
+        u = [ecu.a, ecu.df, imu.psiDot, imu.ax, imu.ay]
         
         y = np.array([gps.x, gps.y, enc.v_meas, 0.5*u[1]*enc.v_meas])
         
@@ -345,7 +345,7 @@ class Estimator(object):
                 # MultiRate for encoder
                 self.v_meas_count += 1
                 idx.append(2)
-                idx.append(3)
+                # idx.append(3)
             if self.ax_his[-1] == u[3]:
                 self.ax_count += 1
             if self.ay_his[-1] == u[4]:

workspace/src/barc/src/stateEstimatorPsiInput.py
@@ -40,7 +40,7 @@ def main():
     rospy.init_node("state_estimation")
     a_delay     = 0.0
     df_delay    = 0.0
-    loop_rate   = 50.0
+    loop_rate   = 100.0
    	
     # Tuning for estimator at high speed
     Q_hs = eye(7)
@@ -184,7 +184,16 @@ def main():
                       inp_psiDot_his    = est.psiDot_his,
                       inp_a_his         = est.inp_a_his,
                       inp_df_his        = est.inp_df_his,
-                      flagVy            = saved_switch)
+                      flagVy            = saved_switch,
+                      roll_his          = est.roll_his,
+                      pitch_his         = est.pitch_his,
+                      wx_his            = est.wx_his,
+                      wy_his            = est.wy_his,
+                      wz_his            = est.wz_his,
+                      v_rl_his          = est.v_rl_his,
+                      v_rr_his          = est.v_rr_his,
+                      v_fl_his          = est.v_fl_his,
+                      v_fr_his          = est.v_fr_his)
 
     pathSave = os.path.join(homedir,"barc_data/estimator_imu.npz")
     np.savez(pathSave,psiDot_his    = imu.psiDot_his,
@@ -305,6 +314,22 @@ class Estimator(object):
         self.psiDot_his = []
         self.inp_a_his      = []
         self.inp_df_his     = []
+
+        # Angular Velocities
+        self.wx_his     = []
+        self.wy_his     = []
+        self.wz_his     = []
+        
+        # Roll an pitch
+        self.roll_his   = []
+        self.pitch_his  = []
+
+        # Encored Readinds
+        self.v_rl_his   =[]
+        self.v_rr_his   =[]
+        self.v_fl_his   =[]
+        self.v_fr_his   =[]
+
         # COUNTERS FOR PACKAGE LOST
         self.pkg_count    = 0
         self.x_count      = 0
@@ -328,7 +353,19 @@ class Estimator(object):
         u = [self.motor_his.pop(0), self.servo_his.pop(0), imu.psiDot]
         
         y = np.array([gps.x, gps.y, enc.v_meas, imu.ax, imu.ay, 0.5*u[1]*enc.v_meas])
+
+        # Read Measurements which are not used but must be saved
+        wx_imu    = imu.w_x
+        wy_imu    = imu.w_y
+        wz_imu    = imu.w_z
+        roll_imu  = imu.roll
+        pitch_imu = imu.pitch
         
+        v_rl_enc = enc.v_rl
+        v_rr_enc = enc.v_rr
+        v_fl_enc = enc.v_fl
+        v_fr_enc = enc.v_fr
+
         KF(y,u)
 
         # SAVE THE measurement/input SEQUENCE USED BY KF
@@ -341,6 +378,21 @@ class Estimator(object):
         self.inp_a_his.append(u[0])
         self.inp_df_his.append(u[1])
         self.psiDot_his.append(u[2])
+
+        # Angular Velocities
+        self.wx_his.append(wx_imu)
+        self.wy_his.append(wy_imu)
+        self.wz_his.append(wz_imu)
+        
+        # Roll an pitch
+        self.roll_his.append(roll_imu)
+        self.pitch_his.append(pitch_imu)
+
+        # Encored Readinds
+        self.v_rl_his.append(v_rl_enc)
+        self.v_rr_his.append(v_rr_enc)
+        self.v_fl_his.append(v_fl_enc)
+        self.v_fr_his.append(v_fr_enc)
         
         self.gps_time.append(gps.curr_time)
         self.imu_time.append(imu.curr_time)
@@ -381,7 +433,6 @@ class Estimator(object):
 
         # Decide if vy has to be set to zero
         if abs(u[2]) < self.thReset: # was 0.4
-            self.vy_est = 0.0
             self.z[3]   = 0.0
 
         # Now do multirate KF
@@ -497,6 +548,9 @@ class ImuClass(object):
         self.roll    = 0.0
         self.pitch   = 0.0
         self.yaw_raw = 0.0
+        self.w_x     = 0.0
+        self.w_y     = 0.0
+        self.w_z     = 0.0
         
         # Imu measurement history
         self.yaw_his     = [0.0]
@@ -531,14 +585,28 @@ class ImuClass(object):
         self.yaw_raw = np.unwrap([self.yaw_raw_his[-1],yaw_raw])[1]            
 
         w_z = data.angular_velocity.z
+        w_x = data.angular_velocity.x
+        w_y = data.angular_velocity.y
         a_x = data.linear_acceleration.x
         a_y = data.linear_acceleration.y
         a_z = data.linear_acceleration.z
 
-        self.psiDot = w_z
-        # Transformation from imu frame to vehicle frame (negative roll/pitch and reversed matrix multiplication to go back)
-        self.ax = cos(-pitch_raw)*a_x + sin(-pitch_raw)*sin(-roll_raw)*a_y - sin(-pitch_raw)*cos(-roll_raw)*a_z
-        self.ay = cos(-roll_raw)*a_y + sin(-roll_raw)*a_z
+        self.w_x = w_x
+        self.w_y = w_y
+        self.w_z = w_z
+
+
+        # Ask Ugo's notes for transformation 
+
+        # self.psiDot = w_z
+        self.psiDot = sin(roll_raw) / cos(pitch_raw) * w_y + cos(roll_raw) / cos(pitch_raw) * w_z
+
+        # self.ax = cos(-pitch_raw)*a_x + sin(-pitch_raw)*sin(-roll_raw)*a_y - sin(-pitch_raw)*cos(-roll_raw)*a_z
+        self.ax = cos(pitch_raw)*a_x + sin(pitch_raw)*sin(roll_raw)*a_y + sin(pitch_raw)*cos(roll_raw)*a_z
+
+        # self.ay = cos(-roll_raw)*a_y + sin(-roll_raw)*a_z
+        self.ay = cos(roll_raw)*a_y - sin(roll_raw)*a_z
+
 
         self.saveHistory()
         self.yaw_raw -= self.yaw_raw_his[1]       