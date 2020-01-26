b3656098f656ac30445ddeaa49eb1aabcfca1493
lujieyang <47341031+lujieyang@users.noreply.github.com>
2020-01-25 20:21:10 -0800

    Adding fill out code for students

--- workspace/src/labs/src/lab1/remapping.py
+++ workspace/src/labs/src/lab1/remapping.py
@@ -4,6 +4,10 @@ from geometry_msgs.msg import Twist
 
 def callback_function(data):
     #FILL IN HERE
+    global publisher_name, msg
+    msg.linear.x = -data.linear.x
+    msg.angular.z = -data.angular.z
+    publisher_name.publish(msg)
 
 
 def subscriber_name():
@@ -11,6 +15,12 @@ def subscriber_name():
     rospy.init_node('subscriber_name', anonymous=True)
 
     #FILL IN HERE
+    global publisher_name,msg
+    msg = Twist()
+    publisher_name = rospy.Publisher('remapped_topic_name',Twist,queue_size = 16)
+    rospy.Subscriber('turtle1/cmd_vel', Twist, callback_function)
+    rospy.Rate(30)
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
